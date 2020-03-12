6530098b69e8724b3f451ed4d6d2d0003bf14967
ae200202 <yaho1024@etoos.com>
2020-03-12 13:52:11 +0900

    first commit

---
+++ src/main/java/team/study/rest/TeamRestApplication.java
@@ -0,0 +1,13 @@
+package team.study.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamRestApplication.class, args);
+    }
+
+}
