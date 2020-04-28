98bc503cc7b54b80b55bc8820988cb3a78c55a45
SOOHYUNLIM <sh_7682@naver.com>
2020-04-28 21:30:34 +0900

    RDS, SQL 파일 로드 설정

--- src/main/java/team/study/TeamRestApplication.java
+++ src/main/java/team/study/TeamRestApplication.java
@@ -3,7 +3,9 @@ package team.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class TeamRestApplication {
