69ff04e48c4f106fb0ebef40f2445975ac999c3b
SOOHYUNLIM <sh_7682@naver.com>
2020-04-29 09:26:45 +0900

    @EnableJpaAuditing 삭제

--- src/main/java/team/study/TeamRestApplication.java
+++ src/main/java/team/study/TeamRestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class TeamRestApplication {
