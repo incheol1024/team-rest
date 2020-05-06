58bc3a7a853d88dab94e088c2fe081c411fb68b8
SOOHYUNLIM <sh_7682@naver.com>
2020-05-06 22:30:23 +0900

    RDS 설정

--- src/test/java/team/study/rest/sample/service/HibernateRepositoryTests.java
+++ src/test/java/team/study/rest/sample/service/HibernateRepositoryTests.java
@@ -2,11 +2,14 @@ package team.study.rest.sample.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import team.study.rest.sample.dto.SampleDto;
 import team.study.rest.sample.repository.HibernateRepository;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)  //실제 DB 사용
 public class HibernateRepositoryTests {
 
     @Autowired
